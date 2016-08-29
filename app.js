99266a5727307b91f15bc971e5ff3b0e5b796160
maryscode <maryschan@gmail.com>
2016-08-29 15:18:22 -0400

    add moments module and clean up js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var http = require('http');
 var path = require('path');
 var request = require('request');
 var app = express();
+var moment = require('moment');
+moment().format();	
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -25,23 +27,40 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Convert date to time ago
+var convertDates = function(e){
+	for(var i = 0; i < e.length; i++) {
+		var timeStamp = e[i].publish_at;
+		var timeString = String(timeStamp);
+		var timeAgo = moment(timeString, "YYYY-MM-DD h:mm:ss").fromNow();
+		e[i].publish_at = timeAgo;
+	}
+};
+
+var articleData1 = require('./data/articles');
+var articleData2 = require('./data/more-articles');
+var articleTotal = articleData1.length + articleData2.length;
+
 // first route
 app.get('/', function(req, res) {
-	res.render('index')
+	res.render('index',
+		{ articleTotal : articleTotal }
+	)
 });
+
+
 
 // second route
 app.get('/loadarticles', function(req, res){
-	var articleData = require('./data/articles');
-	res.json(articleData);
+	convertDates(articleData1);
+	res.send(articleData1);
 });
 
 // third route
 app.get('/morearticles', function(req, res){
-	var articleData = require('./data/more-articles');
-	res.json(articleData);
+	convertDates(articleData2);	
+	res.send(articleData2);
 });
-
 
 // Run on http://localhost:3000
 app.listen(process.env.PORT || 3000, function () {
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,69 +1,55 @@
 /////////////////////////////
 ////    client side     ////
 ///////////////////////////
-
 var articleArray = [];
-var articleArray2 = [];
-var visibleRows = 0;
+var visibleRows = 0; // Visible rows in DOM
+var articleCount = 0; // Total number of articles from currently loaded data (updates as you load second json file)
 var extraRows = 0; // Additional rows from second file
-var articleCount; // Total number of articles from requested files (updates as you load second json file)
-var load = 10; // Total number of articles to load at a time
+var load = 10; // Set number of articles to load at a time here
+
+var countRows = function(){
+	visibleRows = $('tr.row').length;
+}
 
 // Add more articles from first file
 var addArticles = function(a){
-//	visibleRows = $('#articlesTable>tbody>tr:visible').length;	
-	var i = visibleRows + a;  
-	var displayArticles = articleArray.slice(0,i);
-	$("tbody").html(displayArticles);
+	countRows();
+	var i = visibleRows + a;
+	var displayArticles = articleArray.slice(visibleRows,i);
+	$("tbody").append(displayArticles);
 };
+
+var buildRows = function(data, rowclass){
+	articleCount = articleCount + data.length; // initial: article count = 30; data.length = 30
+	// second: articleCount = 60;
+	for(var i = 0; i < data.length; i++) { 
+		// initial: for i = 0 i < 30 
+		// second: for i = 0; i < 30
+		var count = i + 1;
+		var tblRow = "<tr class='row " + rowclass + "'>" + 
+		"<td>" + count + "</td>" +			
+		"<td>" + data[i].title + "</td>" +
+		"<td>" + data[i].profile.first_name + " " + data[i].profile.last_name + "</td>" + 
+		"<td>" + data[i].words + "</td>" + 
+		"<td>" + data[i].publish_at + "</td>" +
+		"</tr>"
+		articleArray.push(tblRow)
+	}
+}
 
 // Load the initial set of articles
 var initialLoad = function(){
 	$.get( '/loadarticles', function(data) {
-		articleCount = data.length;	
-		for(var i = 0; i < data.length; i++) {
-			var count = i + 1;
-			var tblRow = "<tr>" + 
-			"<td>" + count + "</td>" +			
-			"<td>" + data[i].title + "</td>" +
-			"<td>" + data[i].profile.first_name + " " + data[i].profile.last_name + "</td>" + 
-			"<td>" + data[i].words + "</td>" + 
-			"<td>" + data[i].publish_at + "</td>" + 
-			"</tr>"
-			articleArray[i] = tblRow
-		}
+		buildRows(data,"");
 		addArticles(load);
-	});      	
+	});
 };
-
-
-
-// Add article amount function
-var addArticles2 = function(a){
-	var i = extraRows + a;
-	var displayArticles = articleArray2.slice(extraRows,i);
-	$("tbody").append(displayArticles);
-	
-};
-
 
 // Load second set of articles
 var loadSet2 = function(){
 	$.get( '/morearticles', function(data) {
-		articleCount = articleCount + data.length;	
-		//console.log("updated articleCount: " + articleCount);
-		for(var i = 0; i < data.length; i++) {
-			var count = i + 1;
-			var tblRow = "<tr class='extra'>" + 
-			"<td>new " + count + "</td>" +
-			"<td>" + data[i].title + "</td>" +
-			"<td>" + data[i].profile.first_name + " " + data[i].profile.last_name + "</td>" + 
-			"<td>" + data[i].words + "</td>" + 
-			"<td>" + data[i].publish_at + "</td>" + 
-			"</tr>"
-			articleArray2[i] = tblRow
-		}
-		addArticles2(load);
+		buildRows(data, "extra");
+		addArticles(load);
 	});
 	window.setTimeout(updateSortTable, 1000)
 };
@@ -71,59 +57,52 @@ var loadSet2 = function(){
 var moreButton = function(){
 	$("#btnLoadMore").click(function(){
 		updateSortTable();
+		countRows();
+		extraRows = $('tr.extra').length;
 
-		visibleRows = $('#articlesTable>tbody>tr:visible').length;
-		extraRows = $('#articlesTable>tbody>tr.extra:visible').length;
 		var maxArticles = articleCount - load; // defines second to last load
-		//console.log("visibleRows: " + visibleRows + "\narticleCount: " + articleCount + "\nextraRows: " + extraRows + "\nmaxArticles: " + maxArticles);		
 		
 		// Hide load more button if on the second to last load
 		if ((visibleRows == maxArticles ) && (extraRows > 0) ) { // if visibleRows = 50 and maxArticles = 50
 			$("#btnLoadMore").hide();
 		}	
-			
 
 		// if initial data 
 		if ((visibleRows < articleCount) && ( extraRows == 0 )) { // if rows visible is less than file1 total, then keep adding from first file
+			console.log("more from first set");
 			addArticles(load);
+			console.log(articleArray[40])
+
 		}
 		else if ((visibleRows = articleCount) && (extraRows == 0)) { // if  visible and equal to file1 total, then load next data
+			console.log("load second set");			
 			loadSet2(); // after click: now visibleRows = 40; articleCount = 60;  extraRows = 10 
+
 		}
 		// if second set has been loaded but not more than total articles, show more from json file 2
 		else if ((visibleRows <= articleCount ) && (extraRows > 0) ) { // extraRows > 0 basically means the second set has been loaded
-			addArticles2(load); 
-		}
+			console.log("more from second set");			
+			addArticles(load); 
+		};
 
 	});
 };
 // Sort Table; rows must be fully loaded
 var sortTable = function(){
+
 	$("#articlesTable").tablesorter({
 	  headers: { 
 	      // Disable first and second columns (starts at 0)
-/*	      0: { sorter: false }, 
+	      0: { sorter: false }, 
 	      1: { sorter: false },
-	      2: { sorter: false }        */
+	      2: { sorter: false }        
 	  }
 	}); 	
 };
 // Trigger sort function when new data is added
 var updateSortTable = function(){
 	$("#articlesTable").trigger("update"); 
-
-  // bind to sort events
-  /*$("#articlesTable")
-    .bind("sortStart",function() {
-    	alert("start!");
-    })
-    .bind("sortEnd",function() {
-    	alert("end!");
-    });
-*/
-
 }
-
 
 // Initiate Functions
 initialLoad();
